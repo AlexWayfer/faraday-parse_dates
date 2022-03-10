58e33dc86549a0a873006705e82b9e61dfdad5bd
Alexander Popov <alex.wayfer@gmail.com>
2022-03-11 00:32:13 +0300

    Resolve RuboCop offenses in specs

--- spec/faraday/parse_dates/middleware_spec.rb
+++ spec/faraday/parse_dates/middleware_spec.rb
@@ -1,40 +1,91 @@
 # frozen_string_literal: true
 
 RSpec.describe Faraday::ParseDates::Middleware, type: :response do
-  let(:parsed) { Time.utc(2012, 2, 1, 13, 14, 15) }
+  subject { process(body).body&.dig(*body_keys) }
 
-  it 'parses dates' do
-    expect(process('x' => '2012-02-01T13:14:15Z').body['x']).to eq(parsed)
-    expect(process('x' => '2012-02-01T15:14:15+02:00').body['x'].utc).to eq(parsed)
-    expect(process('x' => '2012-02-01T11:14:15-0200').body['x'].utc).to eq(parsed)
+  let(:body) { { 'x' => x } }
+  let(:body_keys) { ['x'] }
+
+  let(:parsed) { Time.utc(2012, 2, 1, 13, 14, 15, parsed_microseconds) }
+  let(:parsed_microseconds) { 0 }
+
+  context 'with correct dates' do
+    context 'without microseconds' do
+      context 'with Zulu timezone offset' do
+        let(:x) { '2012-02-01T13:14:15Z' }
+
+        it { is_expected.to eq parsed }
+      end
+
+      context 'with positive timezone offset' do
+        let(:x) { '2012-02-01T15:14:15+02:00' }
+
+        it { is_expected.to eq parsed }
+      end
+
+      context 'with negative timezone offset' do
+        let(:x) { '2012-02-01T11:14:15-0200' }
+
+        it { is_expected.to eq parsed }
+      end
+    end
+
+    context 'with microseconds' do
+      let(:parsed_microseconds) { 123_000 }
+
+      context 'with Zulu timezone offset' do
+        let(:x) { '2012-02-01T13:14:15.123Z' }
+
+        it { is_expected.to eq parsed }
+      end
+
+      context 'with negative timezone offset' do
+        let(:x) { '2012-02-01T11:14:15.123-02:00' }
+
+        it { is_expected.to eq parsed }
+      end
+    end
+
+    context 'when inside Hash' do
+      let(:body_keys) { super().push('y') }
+
+      let(:x) { { 'y' => '2012-02-01T13:14:15Z' } }
+
+      it { is_expected.to eq parsed }
+    end
+
+    context 'when inside Array' do
+      let(:body_keys) { super().push(0, 'y') }
+
+      let(:x) { [{ 'y' => '2012-02-01T13:14:15Z' }] }
+
+      it { is_expected.to eq parsed }
+    end
   end
 
-  it 'parses dates with milliseconds' do
-    date_str = '2012-02-01T13:14:15.123Z'
-    non_zulu_date_str = '2012-02-01T11:14:15.123-02:00'
-    expect(process('x' => date_str).body['x']).to eq(Time.parse(date_str))
-    expect(process('x' => non_zulu_date_str).body['x']).to eq(Time.parse(date_str))
+  context 'when body is nil' do
+    let(:body) { nil }
+
+    it { is_expected.to be_nil }
   end
 
-  it 'parses nested dates in hash' do
-    expect(process('x' => { 'y' => '2012-02-01T13:14:15Z' }).body['x']['y']).to eq(parsed)
+  context 'when Arrays has no Time strings' do
+    let(:x) { [1, 2, 3] }
+
+    it { is_expected.to eq x }
   end
 
-  it 'parses nested dates in arrays' do
-    expect(process('x' => [{ 'y' => '2012-02-01T13:14:15Z' }]).body['x'][0]['y']).to eq(parsed)
-  end
+  context 'with incorrect Time-like strings' do
+    [
+      '2012-02-01T13:14:15Z bla',
+      '12012-02-01T13:14:15Z',
+      "2012-02-01T13:14:15Z\nfoo"
+    ].each do |string|
+      context "when String is #{string}" do
+        let(:x) { string }
 
-  it 'returns nil when body is empty' do
-    expect(process(nil).body).to be_nil
-  end
-
-  it 'leaves arrays with ids alone' do
-    expect(process('x' => [1, 2, 3]).body).to eq('x' => [1, 2, 3])
-  end
-
-  it 'does not parse date-like things' do
-    expect(process('x' => '2012-02-01T13:14:15Z bla').body['x']).to eq '2012-02-01T13:14:15Z bla'
-    expect(process('x' => '12012-02-01T13:14:15Z').body['x']).to eq '12012-02-01T13:14:15Z'
-    expect(process('x' => "2012-02-01T13:14:15Z\nfoo").body['x']).to eq "2012-02-01T13:14:15Z\nfoo"
+        it { is_expected.to eq string }
+      end
+    end
   end
 end
